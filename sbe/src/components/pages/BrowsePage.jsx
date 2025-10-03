8c58c2f86e3b7619d3b4ff95be8d320ba27408da
James Russel E. Adia <adiajames272@gmail.com>
2025-10-04 02:30:27 +0800

    browsepage jsx and css

--- sbe/src/components/pages/BrowsePage.jsx
+++ sbe/src/components/pages/BrowsePage.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-// other imports...
+import '../../styles/pages/BrowsePage.css';
+
+// categories galing csv
+const categories = [
+  'Space Biology',
+  'Bone & Skeletal Research',
+  'Plant Biology',
+  'Biomedical Research',
+  'Bioinformatics',
+  'Cell Biology',
+  'Genetics & Genomics',
+  'Radiation Biology',
+  'Muscle Research',
+  'Stem Cell Research',
+];
 
 export default function BrowsePage() {
+
   return (
-    <div>
-      <p>coming soon... BROWSE PAGE TO</p>
+    <div className="browse-container">
+      <h2 className="browse-title">Biology Categories</h2>
+      <ul className="category-list">
+        {categories.map((cat, idx) => (
+          <li key={idx}>{cat}</li>
+        ))}
+      </ul>
     </div>
   );
 }
